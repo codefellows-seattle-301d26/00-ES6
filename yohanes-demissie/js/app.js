d9851273c5f8ecb516057d5d121bd901c54619ea
Yohanes Demissie <Yohanesdemissie@gmail.com>
2017-12-04 13:27:05 -0800

    fixed for loops and globals to let instead of const

--- yohanes-demissie/js/app.js
+++ yohanes-demissie/js/app.js
@@ -1,22 +1,24 @@
 'use strict';
 
 let names = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
+// let is being used here becuase it is being called back into an object cont
 
 let allProducts = [];
-let container = document.getElementById('image_container');
-let viewed = [];
-let labels = [];
-let pics = [document.getElementById('left'),
+const container = document.getElementById('image_container');
+const viewed = [];
+const labels = [];
+const pics = [document.getElementById('left'),
 document.getElementById('center'), //eslint-disable-line
 document.getElementById('right')]; //eslint-disable-line
-let list = document.getElementById('productlist');
+const list = document.getElementById('productlist');
 let totalClicks = 0;
-let views = [];
-let votes = [];
+const views = [];
+const votes = [];
 
 function Product(name) {
   this.name = name;
   this.path = 'img/' + name + '.jpg';
+  //`img/ ${name} .jpg`
   this.votes = 0;
   this.views = 0;
   allProducts.push(this);
@@ -29,20 +31,20 @@ function makeRandom() {
 function displayPics() {
   // roll for three random indexes
   while(viewed.length < 6) {
-    let rando = makeRandom();
+    const rando = makeRandom();
     while(!viewed.includes(rando)) {
       viewed.push(rando);
     }
   }
 
-  // TODO: In a sentence or two, explain why the previous line of code threw an error when we changed the variable declaration from `let to `let`.
-  //Because let is a local variable not a global variable
+  // TODO: In a sentence or two, explain why the previous line of code threw an error when we changed the variable declaration from `const to `const`.
+  //Because const is a local variable not a global variable
 
   console.log(viewed);
 
   // To the DOM and beyond!
   for(let i = 0; i < 3; i++) {
-    let temp = viewed.shift();
+    const temp = viewed.shift();
     pics[i].src = allProducts[temp].path;
     pics[i].id = allProducts[temp].name;
     allProducts[temp].views += 1;
@@ -73,7 +75,7 @@ function handleClick(event) {
 
 function showList() {
   for(let i = 0; i < allProducts.length; i++) {
-    let liEl = document.createElement('li');
+    const liEl = document.createElement('li');
     liEl.textContent = allProducts[i].name + ' has ' + allProducts[i].votes + ' votes in ' + allProducts[i].views + ' views';
     list.appendChild(liEl);
   }
@@ -89,7 +91,7 @@ function makeChartData() {
 
 function makeChart() {
   makeChartData();
-  let ctx = document.getElementById('chartypants').getContext('2d');
+  const ctx = document.getElementById('chartypants').getContext('2d');
   new Chart(ctx, {
     type: 'bar',
     data: {
@@ -114,7 +116,7 @@ function makeChart() {
       }
     }
   });
-  Chart.defaults.global.defaultFontColor = '#eee';  
+  Chart.defaults.global.defaultFontColor='#eee';
 }
 
 container.addEventListener('click', handleClick);
